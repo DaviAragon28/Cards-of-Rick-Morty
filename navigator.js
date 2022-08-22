00c3ee640e1ad596c581a442d98c2e645e1fcc35
David Aragón <Davidaragon0007@gmail.com>
2022-08-21 21:49:20 -0500

    Terminando

--- navigator.js
+++ navigator.js
@@ -26,43 +26,15 @@ window.addEventListener('hashchange', knowHashPage, false)
 window.addEventListener('scroll', infiniteScroll, false)
 
 
-filtered.addEventListener('click', () => {
-    location.hash = '#filteredSearch'
-})
 
 titleHome.addEventListener('click', () => {
    location.hash = '#home'
 })
 
 
-// buttonFilter.addEventListener('click', () => {
-//     newFunction() 
-// })
-
-// const newFunction = () => {   
-//     let valueGender = getValueInputSelectGender.options[getValueInputSelectGender.selectedIndex].text;
-//     let valueStatus = getValueInputSelectStatus.options[getValueInputSelectStatus.selectedIndex].text;
-//     let valueSpecies= getValueInputSelectSpecies.options[getValueInputSelectSpecies.selectedIndex].text;
-//     if (valueGender === 'All') {
-//         valueGender = ''
-//     } if (valueStatus === 'All') {
-//         valueStatus = ''
-//     } if (valueSpecies === 'All') {
-//         valueSpecies = ''
-//     }
-//     console.log(valueGender, valueStatus, valueSpecies);
-//     // getCharactersRickAndMorty(valueGender, valueStatus, valueSpecies)
-// }
-
-const pageToFilter = () => {
-    containerAllCards.classList.add('inactive');
-    filtered.classList.add('inactive');
-    containerFiltered.classList.remove('inactive')
-}
-
 const pageHome = () => {
     containerAllCards.classList.remove('inactive');
-    filtered.classList.remove('inactive');
+    // filtered.classList.remove('inactive');
     containerFiltered.classList.add('inactive')
     infiniteScroll = getInfiniteScroll;
 }
